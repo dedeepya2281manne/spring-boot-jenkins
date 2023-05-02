9b414fbca2135dfcd2eb1c336e7e9830e7a54eec
MANNE DEDEEPYA <dedeepyamanne02@gmail.com>
2023-05-02 11:00:06 +0530

    application for jenkins

---
+++ src/test/java/com/demo/jenkins/springbootjenkins/SpringBootJenkinsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.demo.jenkins.springbootjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test cases  executing...!!");
+	}
+
+}
